1f18ca8b04c5fe40c7d49c17f872e7da932a7dfd
hyperGT <charlesgabriel102@gmail.com>
2024-08-09 18:51:54 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using calculadora.Models;
+
+Operations op = new();
+
+op.Somar(10, 5);
+
+op.Subtrair(10, 8);
+
+op.Multiplicar(2, 4);
+
+op.Dividir(8, 4);
+
+op.Potencia(2, 10);
+
+op.Seno(30);
+
+op.Coseno(30);
+
+op.Tangente(30);
+
+op.RaizQuadrada(81);
